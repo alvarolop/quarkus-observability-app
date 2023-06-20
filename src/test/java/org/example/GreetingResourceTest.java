98bc9648b51316dd123dfe0cf5d5ff80a54452ac
Alvaro López Medina <alopezme@redhat.com>
2023-06-20 10:31:52 +0200

    :test_tube: Fix Quarkus test

--- src/test/java/org/example/GreetingResourceTest.java
+++ src/test/java/org/example/GreetingResourceTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.Path;
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -12,7 +14,7 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/api/hello")
           .then()
              .statusCode(200)
              .body(is("Hello from RESTEasy Reactive"));
